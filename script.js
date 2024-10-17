d3f332722456cc9a71dc728a4e72a95c779e46e3
Zapallozucchini <juan.clavero24@gmail.com>
2024-10-17 19:52:09 -0300

    actualizacion4
    se comenzo con pagina servicios probando carruseles

--- script.js
+++ script.js
@@ -10,3 +10,21 @@ function changeImage() {
 
 // Cambia la imagen cada 3 segundos
 setInterval(changeImage, 3000);
+// PAGINA Y FUNCION DE SERVICIOS
+let currentIndex = [0, 0, 0]; // Cada índice corresponde a un carrusel
+
+// Función para avanzar a la siguiente imagen
+function nextImage(carruselId) {
+    const carrusel = document.getElementById(`carrusel-${carruselId}`);
+    const images = carrusel.getElementsByTagName('img');
+    currentIndex[carruselId] = (currentIndex[carruselId] + 1) % images.length;
+    carrusel.style.transform = `translateX(${-currentIndex[carruselId] * 300}px)`;
+}
+
+// Función para retroceder a la imagen anterior
+function prevImage(carruselId) {
+    const carrusel = document.getElementById(`carrusel-${carruselId}`);
+    const images = carrusel.getElementsByTagName('img');
+    currentIndex[carruselId] = (currentIndex[carruselId] - 1 + images.length) % images.length;
+    carrusel.style.transform = `translateX(${-currentIndex[carruselId] * 300}px)`;
+}
